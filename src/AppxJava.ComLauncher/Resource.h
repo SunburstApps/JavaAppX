a9c2823751d788637650a9ddcb1ce9860f3be963
William Kent <wjk011@gmail.com>
2017-04-12 20:59:16 -0400

    Fill out the RC file for the ComLaucher projects

--- src/AppxJava.ComLauncher/Resource.h
+++ src/AppxJava.ComLauncher/Resource.h
@@ -2,12 +2,10 @@
 // Microsoft Visual C++ generated include file.
 // Used by AppxJava.ComLauncher.rc
 //
-
-#define IDS_PROJNAME                    100
-#define IDR_APPXJAVACOMLAUNCHER	101
+#define IDR_APPXJAVACOMLAUNCHER         101
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        201
