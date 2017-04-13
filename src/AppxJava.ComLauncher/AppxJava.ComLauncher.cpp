e3f129cf9156460a19ad14656fca238a2c2ec559
William Kent <wjk011@gmail.com>
2017-04-12 20:19:14 -0400

    Reformat AppxJava.ComLauncher.cpp

--- src/AppxJava.ComLauncher/AppxJava.ComLauncher.cpp
+++ src/AppxJava.ComLauncher/AppxJava.ComLauncher.cpp
@@ -1,29 +1,19 @@
-// AppxJava.ComLauncher.cpp : Implementation of WinMain
-
-
 #include "stdafx.h"
 #include "resource.h"
 #include "AppxJavaComLauncher_i.h"
 
-
 using namespace ATL;
-
 
 class CAppxJavaComLauncherModule : public ATL::CAtlExeModuleT< CAppxJavaComLauncherModule >
 {
-public :
+public:
 	DECLARE_LIBID(LIBID_AppxJavaComLauncherLib)
 	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_APPXJAVACOMLAUNCHER, "{D678A3FB-9701-44B0-B585-1A340286FBD8}")
-	};
+};
 
 CAppxJavaComLauncherModule _AtlModule;
 
-
-
-//
-extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, 
-								LPTSTR /*lpCmdLine*/, int nShowCmd)
+extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTSTR /*lpCmdLine*/, int nShowCmd)
 {
 	return _AtlModule.WinMain(nShowCmd);
 }
-
