bad01dd9a0d3c711a48aaef7181f3d6f42b6edea
William Kent <wjk@users.noreply.github.com>
2019-05-03 21:50:37 -0400

    Add error message to ComServer WinMain method

--- src/JavaAppX.ComServer/WinMain.cpp
+++ src/JavaAppX.ComServer/WinMain.cpp
@@ -2,5 +2,10 @@
 
 _Use_decl_annotations_
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE /* hPrevInstance */, LPWSTR cmdLine, int nCmdShow) {
+	if (wcscmp(cmdLine, L"/Embedding") != 0) {
+		::MessageBoxW(HWND_DESKTOP, L"This application is not intended to be run directly. It will be launched by the system automatically when needed.", L"OpenJDK for Windows 10", MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
 	return 0;
 }
